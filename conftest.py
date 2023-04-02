cc6f5a1fcf40d23e7dfdbae50678fe0ae7328653
Наталья <azimut_natalia@mail.ru>
2023-04-02 16:18:40 +0200

    Initial commit

---
+++ conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture()
+def browser():
+    try:
+        browser = webdriver.Chrome()
+        yield browser
+    finally:
+        browser.quit()
